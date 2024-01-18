5b37047b5ed4f19bace1544f82d99de1ff89ed7f
Thiago Andreotti <dmpk02@gmail.com>
2024-01-18 17:56:52 -0300

    1st Project

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,56 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import styles from "./page.module.scss";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import useMousePosition from "./utils/useMousePosition";
 
 export default function Home() {
+  const [isHovered, setIsHovered] = useState(false);
+  const { x, y } = useMousePosition();
+  const size = isHovered ? 400 : 40;
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <motion.div
+        className={styles.mask}
+        animate={{
+          WebkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`,
+          WebkitMaskSize: `${size}px`,
+        }}
+        transition={{ type: "tween", ease: "backOut", duration: 0.5 }}
+      >
+        <div className={styles.bgImage}>
+          <div
+            className={styles.image}
+            onMouseEnter={() => {
+              setIsHovered(true);
+            }}
+            onMouseLeave={() => {
+              setIsHovered(false);
+            }}
+          ></div>
+        </div>
+      </motion.div>
+
+      <div className={styles.body}>
+        <div className={styles.bgImage}>
+          <div className={styles.image}></div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <p className={styles.position1}>EVERY SCENT HAS AN ORIGIN</p>
+      <div className={styles.position2}>
+        <span>IKIGAI • HANAMI • KOMOREBI</span>
+        <p>
+          &rdquo;In spring, when nature awakens from its winter slumber, sakuras
+          bloom like a silent symphony of ephemeral colors. In this temporary
+          realm of dancing petals lies the &lsquo;ikigai&lsquo; of the season —
+          the true reason for being. Just as cherry blossoms unfold delicately,
+          they remind us of life&lsquo;s fleeting beauty and the importance of
+          finding meaning in every moment. Spring is a reminder that, like
+          blossoming sakuras, our &lsquo;ikigai&lsquo; is also revealed in the
+          constant pursuit of growth and renewal.&rdquo;;
+        </p>
       </div>
     </main>
-  )
+  );
 }
